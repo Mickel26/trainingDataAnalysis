a2994ef1b306de9d450f29a9d13e6f6eae7aac13
Mickel26 <mikolajfilka@gmail.com>
2026-09-14 20:16:20 +0200

    add ETL script for loading and processing summarized activities

---
+++ src/etl.py
@@ -0,0 +1,37 @@
+import json
+import glob
+import sqlite3
+
+def load_summarized_activities(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        activities = data[0]["summarizedActivitiesExport"]
+        print(f"Loaded {len(activities)} summarized activities from JSON file.")
+    return activities
+
+def load_all_activities(folder_path):
+    files = glob.glob(f"{folder_path}/*_summarizedActivities.json")
+    data = []
+    
+    for (file_path) in files:
+        data.extend(load_summarized_activities(file_path))
+        
+    return data
+        
+    
+all_activities = load_all_activities("data\\raw")
+print(f"Total activities: {len(all_activities)}")
+
+def create_db(db_path):
+    conn = sqlite3.connect(db_path)
+    
+    conn.execute('''CREATE TABLE IF NOT EXISTS activities (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        type TEXT
+        )''')
+    conn.commit()
+    
+    return conn
+
+conn = create_db("data\\processed\\activities.db")
